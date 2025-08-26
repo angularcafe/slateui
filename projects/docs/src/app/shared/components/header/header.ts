3c6064ace425e0070650aee40990699b848185fc
Mohammad Javed <immohammadjaved@gmail.com>
2025-08-26 19:16:55 +0530

    docs(icon): Add bootstrap icon

--- projects/docs/src/app/shared/components/header/header.ts
+++ projects/docs/src/app/shared/components/header/header.ts
@@ -2,13 +2,14 @@ import { Component, inject, model } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ThemeService } from '@slateui/theme';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideGithub, lucideMenu, lucideSun, lucideTwitter, lucideMoon, lucideX } from '@ng-icons/lucide';
+import { lucideMenu, lucideSun, lucideMoon } from '@ng-icons/lucide';
+import { bootstrapGithub, bootstrapTwitterX } from '@ng-icons/bootstrap-icons'
 import { UiButton } from 'ui';
 
 @Component({
   selector: 'docs-header',
   imports: [NgIcon, UiButton, RouterLink],
-  viewProviders: [provideIcons({ lucideGithub, lucideTwitter, lucideMenu, lucideSun, lucideMoon })],
+  viewProviders: [provideIcons({ lucideMenu, lucideSun, lucideMoon, bootstrapGithub, bootstrapTwitterX })],
   templateUrl: './header.html'
 })
 export class Header {
