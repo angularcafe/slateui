a3d4ca56e4843c5323e973c210a8261677dbadef
Mohammad Javed <immohammadjaved@gmail.com>
2025-08-28 17:42:34 +0530

    UI(table): Add table directive

---
+++ projects/docs/src/app/pages/docs/components/table/table.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ComponentPreview } from '@components/component-preview/component-preview';
+import { tableVariants, tableMeta } from './table.variants';
+
+@Component({
+  selector: 'docs-table',
+  standalone: true,
+  imports: [ComponentPreview],
+  template: `
+    <docs-component-preview 
+      [meta]="tableMeta" 
+      [variants]="tableVariants">
+    </docs-component-preview>
+  `
+})
+export class Table {
+  tableMeta = tableMeta;
+  tableVariants = tableVariants;
+}
