633d350e3616cbee9ac5f10568e24194932c4f1c
Mohammad Javed <immohammadjaved@gmail.com>
2025-08-29 02:41:23 +0530

    UI(input): Add input directive

---
+++ projects/docs/src/app/pages/docs/components/input/input.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ComponentPreview } from '@components/component-preview/component-preview';
+import { inputVariants, inputMeta } from './input.variants';
+
+@Component({
+  selector: 'docs-input',
+  imports: [ComponentPreview],
+  template: `
+    <docs-component-preview 
+      [meta]="inputMeta" 
+      [variants]="inputVariants">
+    </docs-component-preview>
+  `
+})
+export class Input {
+  inputMeta = inputMeta;
+  inputVariants = inputVariants;
+}
